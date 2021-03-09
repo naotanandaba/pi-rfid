c88b3ac9e33f82651a790361a1f271c8fc077118
Xavi <xguinotv@gmail.com>
2021-03-09 17:01:57 +0100

    first

--- Rfid-rc522.py
+++ Rfid-rc522.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import os
+from mfrc522 import SimpleMFRC522
+
+class Rfid...:
+        # return uid in hexa str
+        def read_uid(self):
+                reader = SimpleMFRC522()
+                uid = reader.read_uid()
+                uid_hex = hex(uid).upper()
+                return uid_hex
+if __name__ == "__main__":
+# rf = Rfid...()
+        try:
+                rf = Rfid_rc522()
+                uid = rf.scan_uid()
+	finally:
+		GPIO.cleanup()
 
