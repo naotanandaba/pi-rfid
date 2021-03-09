3fca668945ed6abdf82a0c45b44b5ffb8e8e100a
Xavi <xguinotv@gmail.com>
2021-03-09 17:24:53 +0100

    almost end rfid-rc522 class

--- Rfid-rc522.py
+++ Rfid-rc522.py
@@ -5,17 +5,22 @@ import os
 from mfrc522 import SimpleMFRC522
 
 class Rfid_rc522:
-        # return uid in hexa str
         def read_uid(self):
                 reader = SimpleMFRC522()
                 uid = reader.read_uid()
                 uid_hex = hex(uid).upper()
                 return uid_hex
 if __name__ == "__main__":
-# rf = Rfid...()
-        try:
-                rf = Rfid_rc522()
-                uid = rf.scan_uid()
-	finally:
-		GPIO.cleanup()
-
+	stop=" "
+	while(stop!="enter"):
+		os.system("clear")
+        	try:
+			print("Scan your card","\t")
+                	rf = Rfid_rc522()
+                	uid = rf.scan_uid()
+			print("Your card number is:","\t")
+			print(uid.strip("0x"),"\t")
+		finally:
+			stop="enter"
+			stop = input("Scan again")
+			GPIO.cleanup()
