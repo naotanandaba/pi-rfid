900ac7e6e4cc2c3e00ef49692e498949111362be
Xavi <xguinotv@gmail.com>
2021-03-09 17:49:23 +0100

    almost end rfid_rc522

--- Rfid-rc522.py
+++ Rfid-rc522.py
@@ -11,16 +11,16 @@ class Rfid_rc522:
                 uid_hex = hex(uid).upper()
                 return uid_hex
 if __name__ == "__main__":
-	stop=" "
-	while(stop!="enter"):
-		os.system("clear")
-        	try:
-			print("Scan your card","\t")
-                	rf = Rfid_rc522()
-                	uid = rf.scan_uid()
-			print("Your card number is:","\t")
-			print(uid.strip("0x"),"\t")
-		finally:
-			stop="enter"
-			stop = input("Scan again")
-			GPIO.cleanup()
+stop=""
+while(stop!="enter"):
+os.system("clear")
+try:
+print("Scan your card","\t")
+        rf = Rfid_rc522()
+        uid = rf.scan_uid()
+print("Your card number is:","\t")
+print(uid.strip("0x"),"\t")
+finally:
+stop="enter"
+stop = input("Scan again")
+GPIO.cleanup()
