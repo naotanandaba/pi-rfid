2056ad474af3b64b219d916b72301c26e48ef052
Xavi <xguinotv@gmail.com>
2021-04-07 12:35:37 +0200

    final

--- Rfid-rc522.py
+++ Rfid-rc522.py
@@ -1,27 +1,25 @@
-
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
 import os
-from mfrc522 import SimpleMFRC522
-
+from mfrc522 import *
 class Rfid_rc522:
-        def read_uid(self):
-                reader = SimpleMFRC522()
-                uid = reader.read_uid()
-                uid_hex = hex(uid).upper()
-                return uid_hex
+    def scan_uid(self):
+            reader = SimpleMFRC522()
+            uid = reader.read_id()
+            uid_hex = hex(uid).upper()
+            return uid_hex
 if __name__ == "__main__":
-	str=""
-	while(str!="\n"):
-		os.system("clear")
-		try:
-			print("Scan your card","\t")
-			rf = Rfid_rc522()
-			uid = rf.scan_uid()
-			print("Your card number is:","\t")
-			print(uid.strip("0X"),"\t")
-		finally:
-			str="\n"
-			str = input("Scan again")
-			GPIO.cleanup()
+    opc=""
+    while(opc!="n"):
+        os.system("clear")
+        print("Scan your card","\t")
+        try:
+            rf = Rfid_rc522()
+            uid = rf.scan_uid()
+            print("Your card number is: ","\t")
+            print(uid.strip("0X"),"\t")
+        finally:
+            opc="n"
+            opc = input("Scan again (y/n):")
+            GPIO.cleanup()
